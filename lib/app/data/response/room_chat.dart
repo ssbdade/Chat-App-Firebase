f4762dcb991ae7303ac9827922a7d17b733679cc
ssbdade <misogiacc@gmail.com>
2023-01-05 16:44:16 +0700

    ghep api tin nhan, check addfr

--- lib/app/data/response/room_chat.dart
+++ lib/app/data/response/room_chat.dart
@@ -15,41 +15,34 @@ class RoomChatRepo extends GetxService {
   var id = '';
 
 
-  getRoomChat( List<RoomModel> list) async {
-    await FirebaseFirestore.instance
-        .collection('roomChats')
-    .where("uid2", isEqualTo: firebaseUser!.uid)
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((doc) async {
-        list.add(RoomModel.fromMap(doc,
-            "user1"
-        ));
-      });
-    });
-    await FirebaseFirestore.instance
-        .collection('roomChats')
-        .where("uid1", isEqualTo: firebaseUser!.uid)
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((doc) async {
-        list.add(RoomModel.fromMap(doc, "user2"));
-      });
-    });
-    print("asdasdasdasdasd: ${list[0].userModel!.fullName}");
-  }
-
-
-  getFriendUser(String uid, UserModel res) {
-    FirebaseFirestore.instance.
-    collection('users').where('userId', isEqualTo: uid)
-        .get().then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((doc) {
-        res =  UserModel.fromMap(doc);
-        Logger.info(res.fullName.toString());
+  getRoomChat(List<RoomModel> list) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('roomChats')
+          .where("uid2", isEqualTo: firebaseUser!.uid)
+          .get()
+          .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) async {
+          list.add(RoomModel.fromMap(doc,
+            "user1",
+            doc.id,
+          ));
         });
-      }
-    );
+      });
+      await FirebaseFirestore.instance
+          .collection('roomChats')
+          .where("uid1", isEqualTo: firebaseUser!.uid)
+          .get()
+          .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) async {
+          list.add(RoomModel.fromMap(doc, "user2", doc.id));
+        });
+      });
+    }
+    on FirebaseException catch (e) {
+      Logger.info(e.message.toString());
+    }
   }
+
 
 }
--- lib/app/models/room_chat_model.dart
+++ lib/app/models/room_chat_model.dart
@@ -4,26 +4,35 @@ class RoomModel {
   String? uid1;
   String? uid2;
   UserModel? userModel;
+  String? roomId;
+  Map<String, dynamic>? isFriends;
 
   RoomModel({
     this.uid1,
     this.uid2,
     this.userModel,
+    this.roomId,
+    this.isFriends,
 });
 
 
-  factory RoomModel.fromMap(map, String user) {
+  factory RoomModel.fromMap(map, String user,String roomId) {
     return RoomModel(
       uid1: map["uid1"],
       uid2: map["uid2"],
       userModel: UserModel.fromMap(map[user]),
+      roomId: roomId,
+      isFriends: map["isFriends"],
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toMap(UserModel userModel1, UserModel userModel2) {
     return {
       "uid1": uid1,
       "uid2": uid2,
+      "user1": userModel1.toMap(),
+      "user2": userModel2.toMap(),
+
     };
   }
 }
